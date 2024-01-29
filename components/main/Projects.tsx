8cafd1a89dabacda7b7fc151d6a01f668dfc97f4
alvimm <filipe2012alvim@gmail.com>
2024-01-29 18:31:22 -0300

    Add motion animations to Projects component

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,9 +1,42 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client'
+import { useEffect, useRef } from 'react';
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion';
 import { myProjects } from "@/constants/myProjects"
 import ProjectCard from "../sub/ProjectCard"
+import { motion, useAnimation } from 'framer-motion';
 
 const Projects = () => {
+  const controls = useAnimation();
+  const ref = useRef<HTMLDivElement>(null);;
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        controls.start("visible");
+      }
+    }, { threshold: 0.1 });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className='flex flex-col items-center justify-center py-20' id="projects">
+    <motion.div
+      ref={ref}
+      variants={slideInFromTop}
+      initial="hidden"
+      animate={controls}
+      className='flex flex-col items-center justify-center py-20'
+      id="projects"
+    >
       <h1 className='text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-cyan-300 '>
         My projects
       </h1>
@@ -12,7 +45,10 @@ const Projects = () => {
       >
         “Quality is more important than quantity. One home run is much better than two doubles.”
       </h2>
-      <div className='h-full w-full grid md:grid-cols-3 gap-10 px-10 pb-10'>
+      <motion.div       ref={ref}
+      variants={slideInFromLeft(0.8)}
+      initial="hidden"
+      animate={controls} className='h-full w-full grid md:grid-cols-3 gap-10 px-10 pb-10'>
         {myProjects.map((project) => (
           <ProjectCard
             src={project.image}
@@ -22,10 +58,10 @@ const Projects = () => {
             key={project.title}
           />
         ))}
-      </div>
-    </div>
-  )
-}
+      </motion.div>
+    </motion.div>
+  );
+};
 
-export default Projects
+export default Projects;
 

b120c01072cd4062e4ade8b46604a34653455b84
alvimm <filipe2012alvim@gmail.com>
2024-01-29 14:58:07 -0300

    Add constants for socials, skills, projects, and motion animations

---
+++ constants/index.ts
@@ -0,0 +1,218 @@
+export const Socials = [
+  {
+    name: "LinkedIn",
+    src: "/linkedin.svg",
+    href: "https://www.linkedin.com/in/filipe-alvim",
+  },
+  {
+    name: "GitHub",
+    src: "/github.svg",
+    href: "https://github.com/Alvimm"
+  },
+];
+
+
+export const First_line_skills = [
+  {
+    skill_name: "Html 5",
+    Image: "/html.png",
+    width: 60,
+    height: 60,
+  },
+  {
+    skill_name: "Css",
+    Image: "/css.png",
+    width: 60,
+    height: 60,
+  },
+  {
+    skill_name: "Java Script",
+    Image: "/js.png",
+    width: 60,
+    height: 60,
+  },
+  {
+    skill_name: "Type Script",
+    Image: "/ts.png",
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: "Angular 17",
+    Image: "/angular.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next js",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Vue Js",
+    Image: "/vue.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Vite",
+    Image: "/vite.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Second_line_skills = [
+  {
+    skill_name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Bun",
+    Image: "/bun.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express js",
+    Image: "/express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Python",
+    Image: "/python.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Java",
+    Image: "/java.png",
+    width: 55,
+    height: 55,
+  },
+  {
+    skill_name: "Spring Boot",
+    Image: "/spring.png",
+    width: 60,
+    height: 60,
+  },
+  {
+    skill_name: "Docker",
+    Image: "/docker.webp",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Git",
+    Image: "/git.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Terminal Bash",
+    Image: "/terminal.png",
+    width: 90,
+    height: 90,
+  },
+];
+
+export const Third_line_skills = [
+  {
+    skill_name: "React Native",
+    Image: "/ReactNative.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Expo",
+    Image: "/expo.png",
+    width: 90,
+    height: 90,
+  },
+  {
+    skill_name: "Flutter",
+    Image: "/flutter.png",
+    width: 50,
+    height: 50,
+  },
+  {
+    skill_name: "Figma",
+    Image: "/figma.png",
+    width: 45,
+    height: 45,
+  },
+  {
+    skill_name: "Bootstrap",
+    Image: "/bootstrap.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Styled Components",
+    Image: "/styled.png",
+    width: 60,
+    height: 60,
+  },
+];
+
+
+
+export const Fourth_line_skills = [
+  {
+    skill_name: "Postgre SQL",
+    Image: "/postgre.png",
+    width: 60,
+    height: 60,
+  },
+  {
+    skill_name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 30,
+    height: 30,
+  },
+  {
+    skill_name: "My SQL",
+    Image: "/mysql.png",
+    width: 60,
+    height: 60,
+  },
+  {
+    skill_name: "Graphql",
+    Image: "/graphql.png",
+    width: 70,
+    height: 70,
+  },
+];
+
+
+export const Fifth_line_skills = [
+  {
+    skill_name: "Linux",
+    Image: "/linux.png",
+    width: 60,
+    height: 60,
+  },
+];
---
+++ constants/myProjects.ts
@@ -0,0 +1,50 @@
+export const myProjects = [
+    {
+        image: '/portfolio.png',
+        title: 'My Portfolio',
+        description: 'This is my portfolio website',
+        link: 'https://github.com/Alvimm/my-portfolio'
+    },
+    {
+      image: '/capputeeno.png',
+        title: 'Capputeeno - Frontend Challenge',
+        description: 'Was a challenge proposed by rocketseat in which the objective is to implement an e-commerce for the sale of mugs and t-shirts.',
+        link: 'https://github.com/Alvimm/capputeeno-challenge-next'
+    },
+    {
+      image: '/smartfit.png',
+        title: 'Smart Fit - Frontend Challenge',
+        description: 'Was a challenge proposed by Smart Fit in which the objective is to implement a page to search for open or closed units for consultation and reservation.',
+        link: 'https://github.com/Alvimm/smartfit-challenge-angular'
+    },
+    {
+      image: '/hsapp.png',
+        title: 'Home Service - Full Stack App',
+        description: 'My first React Native (Expo) Full Stack app which has the purpose of learning.',
+        link: 'https://github.com/Alvimm/home-service-app'
+    },
+    {
+      image: '/books.png',
+        title: 'Book Catalogs',
+        description: 'It has an api to store the books and three frontend projects in reactjs, nextjs and angular.',
+        link: 'https://github.com/Alvimm/m2n5'
+    },
+    {
+      image: '/omnifood.png',
+        title: 'Omnifoood - Landing Page',
+        description: 'A landing page for a fictional company that delivers healthy food.',
+        link: 'https://github.com/Alvimm/Omnifood'
+    },
+    {
+      image: '/mapty.png',
+        title: 'Mapty - Workout Tracker',
+        description: 'A workout tracker app that uses the browser geolocation api to track the workouts and save them in the local storage.',
+        link: 'https://github.com/Alvimm/mapty'
+    },
+    {
+      image: '/forkify.png',
+        title: 'Forkify - Recipe App',
+        description: 'A recipe app that uses the food2fork api to search for recipes and save them in the local storage.',
+        link: 'https://github.com/Alvimm/forkify'
+    },
+];

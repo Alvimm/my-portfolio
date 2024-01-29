bed281c7f3fd5f112c36e2e542d04a93333f32df
alvimm <filipe2012alvim@gmail.com>
2024-01-29 18:31:07 -0300

    Update video position in Hero component

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -2,8 +2,8 @@ import HeroContent from "../sub/HeroContent"
 
 const Hero = () => {
   return (
-    <div className="relative flex flex-col h-full w-full" id="about-me">
-      <video autoPlay muted loop className="rotate-180 absolute top-[-340px] left-0 z-[-2] w-full h-full object-cover">
+    <div className="relative flex flex-col h-full w-full" id="home">
+      <video autoPlay muted loop className="rotate-180 absolute md:top-[-340px] top-[-390px] left-0 z-[-2] w-full h-full object-cover">
         <source src="/blackhole.webm" type="video/webm"/>
       </video>
       <HeroContent />
